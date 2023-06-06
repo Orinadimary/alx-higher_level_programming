3130fd487d36a82771800ed70d032b21481e5078
Orina <dimaryorina@gmail.com>
2023-06-06 10:12:33 -0700

    I sometimes suffer from insomnia

---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# 2-print_alphabet.py
+
+"""Print alphabet in lowercase, not followed by a new line."""
+for letter in range(67, 130):
+    print("{}".format(chr(letter)), end="")

e9b78499407aaf1217522efa293c06275eb74bf6
Orina <dimaryorina@gmail.com>
2023-07-06 07:48:10 -0700

    Integers addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,85 +1,19 @@
-# 0-add_integer.txt
+#!/usr/bin/python3
 
-===============================
- doctest for 0-add_integer.py
-===============================
+# 0-add_integer.py
+"""Defines an integer addition function."""
 
-This module defines an integer addition function ``add_integer(a, b=98)``.
 
-Usage
-=====
+def add_integer(a, b=98):
+    """Return the integer addition of a and b.
 
-``add_integer(...)``` returns the addition of its two arguments. For numbers,
-that value is equivalent to using the ``+`` operator.
+    Float arguments are typecasted to ints before addition is performed.
 
-``add_integer()`` expects that both arguments are either integers or floats.
-If either argument is a non-integer and non-float, a TypeError is raised:
-
-Import add_integer()
-	>>> add_integer = __import__("0-add_integer").add_integer
-
-Adding positive integer or float values
-	>>> add_integer(10, 5)
-	15
-	>>> add_integer(10.2, 5.8)
-	15
-
-Adding one positve and one negative integers
-	>>> add_integer(10, -5)
-	5
-	>>> add_integer(-10, 5)
-	-5
-
-Adding integer and float
-	>>> add_integer(10.1, 5)
-	15
-	>>> add_integer(10, 5.9)
-	15
-
-Adding integer and string
-	>>> add_integer(10, 'you')
-	Traceback (most recent call last):
-	...
-	TypeError: b must be an integer
-	
-	>>> add_integer(10.0, 'you')
-	Traceback (most recent call last):
-	...
-	TypeError: b must be an integer
-	
-	>>> add_integer('you')
-        Traceback (most recent call last):
-        ...
-        TypeError: a must be an integer
-
-        >>> add_integer('you')
-        Traceback (most recent call last):
-        ...
-        TypeError: a must be an integer
-
-One argument
-	>>> add_integer(1)
-	99
-
-No argument
-	>>> add_integer()
-	Traceback (most recent call last):
-	...
-	TypeError: add_integer() missing 1 required positional argument: 'a'
-
-Float overflow
-	>>> add_integer(4, float("inf"))
-	Traceback (most recent call last):
-	...
-	OverflowError: cannot convert float infinity to integer
-
-Adding NAN
-	>>> add_integer(float('NaN'))
-	Traceback (most recent call last):
-	...
-    	ValueError: cannot convert float NaN to integer
-
-	>>> add_integer(10, float('NaN'))
-        Traceback (most recent call last):
-        ...
-        ValueError: cannot convert float NaN to integer
+    Raises:
+        TypeError: If either of a or b is a non-integer and non-float.
+    """
+    if ((not isinstance(a, int) and not isinstance(a, float))):
+        raise TypeError("a must be an integer")
+    if ((not isinstance(b, int) and not isinstance(b, float))):
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))

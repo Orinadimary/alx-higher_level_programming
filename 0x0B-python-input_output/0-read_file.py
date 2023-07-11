0694721dddca5c3cd57f93711f53103c011cafa9
Orina <dimaryorina@gmail.com>
2023-07-11 08:42:15 -0700

    Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Definess a function that reads UTF text file"""
+
+
+def read_file(filename=""):
+    """Function that reads a test file UTF.
+    Args:
+    Filename: the file to be read
+
+    Returns: prints the file to stdout
+    """
+    open with(filename, encoding="utf-8") as file:
+        content = file.read()
+        print(content, end" ")

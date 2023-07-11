c1edf7ebfb01f5b2b3e26cc5c35b04815cdd6a7c
Orina <dimaryorina@gmail.com>
2023-07-11 12:28:48 -0700

    To JSON string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Module for 3-to_json_string.py"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """Function that returns JSON string object"""
+    return json.dumps(my_obj)

2400b63e34f8bbc2276d9a8b491315525733d092
Orina <dimaryorina@gmail.com>
2023-07-11 08:51:48 -0700

    Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -10,6 +10,6 @@ def read_file(filename=""):
 
     Returns: prints the file to stdout
     """
-    with open(filename, encoding="utf-8") as file:
-        content = file.read()
+    with open(filename, encoding="utf-8") as f:
+        content = f.read()
         print(content, end'')

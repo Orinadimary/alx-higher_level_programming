f6ad77aa97e50df88502323715358d9f0bdab449
Orinadimary <dimaryorina@gmail.com>
2023-11-13 00:34:29 -0800

    Only sub class of

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,21 +1,28 @@
 #!/usr/bin/python3
 
-"""Defines a function that checks True or Fslse"""
-
+"""
+It is a module to inherit from function.
+"""
 
 def inherits_from(obj, a_class):
-
-
-"""checks if objects is  in a given class.
-
-    Args:
-    obj: Object to check
-    a_class: the class that shpuld be with the object
-
-    Returns:
-    True - if obejct is as the a_class, Otherwise False.
     """
-    if issubclass(type(obj), a_class) and type(obj) != a_class:
-        return True
+    Inherits from function
+
+    args:
+    obj: object to check inheritance
+    a_class: class object inherited from
+
+    Return:
+    boolean: True or False
+    """
+
+    if type(obj)== a_class:
+        return False
+    elif is instance(obj, a_class):
+        return True 
     else:
         return False
+
+
+
+

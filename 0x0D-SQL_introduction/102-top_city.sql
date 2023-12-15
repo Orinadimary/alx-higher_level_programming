951d22e5f7828e4c2d9c572d9a16d029c1eafe44
Orinadimary <dimaryorina@gmail.com>
2023-12-15 04:19:43 -0800

    Temperatures #1

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,6 @@
+-- top 3 average temperature group by city order by avg_temp
+SELECT city, AVG(value) as avg_temp
+FROM temperatures
+WHERE month = 7 or month = 8
+GROUP BY city
+ORDER BY avg_temp DESC LIMIT 3

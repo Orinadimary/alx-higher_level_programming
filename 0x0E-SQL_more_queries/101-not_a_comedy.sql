6bd1232a08e9c310b08c17dc9171379061abd5bc
Orinadimary <dimaryorina@gmail.com>
2023-12-15 08:59:14 -0800

    No Comedy tonight!

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,5 +1,16 @@
--- Average temperature order by city
-SELECT city, AVG(value) as avg_temp
-FROM temperatures
-GROUP BY city
-ORDER BY avg_temp DESC
+-- lists all not commedy show.
+SELECT tv_shows.title
+FROM tv_shows
+LEFT JOIN (
+	SELECT tv_shows.title
+	FROM tv_shows
+     	JOIN tv_show_genres
+     	ON tv_show_genres.show_id = tv_shows.id
+     	JOIN tv_genres
+     	ON tv_genres.id = tv_show_genres.genre_id
+	WHERE tv_genres.name = "Comedy"
+	ORDER BY tv_shows.id
+	) AS comedy_shows 
+ON comedy_shows.title = tv_shows.title
+WHERE comedy_shows.title is NULL
+ORDER BY tv_shows.title;

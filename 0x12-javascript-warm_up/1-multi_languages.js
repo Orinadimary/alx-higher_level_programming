79b590e3ec0ddf64a0684a369947cb0d37d47a54
Orinadimary <dimaryorina@gmail.com>
2024-01-08 10:38:27 -0800

    3 languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const myVar2 = 'Python is cool';
+const myVar3 = 'C is fun';
+const myVar1 = 'JavaScript is amazing';
+console.log(myVar3);
+console.log(myVar2);
+console.log(myVar1);

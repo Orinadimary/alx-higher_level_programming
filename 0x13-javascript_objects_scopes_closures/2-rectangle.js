9f12cd224f10f4fde7652211e13156c0c2dba318
Orinadimary <dimaryorina@gmail.com>
2024-01-09 03:56:24 -0800

    Rectangle #2

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = parseInt(w);
+      this.height = parseInt(h);
+    }
+  }
+};

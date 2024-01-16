7efc662623932383cfac407a14ff236df9b4c8eb
Orinadimary <dimaryorina@gmail.com>
2024-01-16 09:17:13 -0800

    All states via SQLAlchemy

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+this script filter states using ORM
+script should take 3 arguments:
+    mysql username,
+    mysql password,
+    database name
+"""
+
+import sys
+from sqlalchemy import create_engine as e
+from sqlalchemy.orm import sessionmaker
+from model_state import State, Base
+
+if __name__ == "__main__":
+    engine = e("mysql+mysqldb://{}:{}@localhost/{}"
+               .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+               pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for state in session.query(State).order_by(State.id):
+        print("{}: {}".format(state.id, state.name))

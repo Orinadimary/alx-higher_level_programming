96503da4070f3b64985a278c4da3b87ccfa58c0c
Orinadimary <dimaryorina@gmail.com>
2024-01-18 22:44:01 -0800

    Update a state

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+this script filter states using ORM
+script should take 3 arguments:
+    mysql username,
+    mysql password,
+    database name
+"""
+
+import sys
+from sqlalchemy import create_engine as e
+from sqlalchemy.orm import sessionmaker
+from model_state import State, Base
+
+if __name__ == "__main__":
+    engine = e("mysql+mysqldb://{}:{}@localhost/{}"
+               .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+               pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    update_state = session.query(State).filter_by(id=2).first()
+    update_state.name = 'New Mexico'
+    session.commit()

c5e32e1471c862f257f8c440281eb711e757a327
root <dimaryorina@gmail.com>
2024-02-27 09:53:38 -0800

    First state model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3
 """
-This is the model city  module
+This is the model base module
 """
 
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 
-class City(Base):
+class State(Base):
     """
-    City object that inherits from Base
+    State object that inherits from Base
         __tablename__: mysql table name
         id: unique primary key
-        name: city name
-        state_id: foreign key from the states table
+        name: State name
     """
-    __tablename__ = 'cities'
+    __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
-    name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    name = Column(String(128), nullable=False

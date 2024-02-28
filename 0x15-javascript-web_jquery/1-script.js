3f260096c72b5aa3a384c5caeea2ec8f6e8a8838
root <dimaryorina@gmail.com>
2024-02-28 09:47:50 -0800

    With JQuery

--- 0x15-javascript-web_jquery/1-script.js
+++ 0x15-javascript-web_jquery/1-script.js
@@ -1,12 +1,3 @@
-#!/usr/bin/node
-
-const fs = require('fs');
-
-const path = process.argv[2];
-const content = process.argv[3];
-
-fs.writeFile(path, content, 'utf8', (err) => {
-  if (err) {
-    console.error(err);
-  }
+$(document).ready(function () {
+  $('header').css('color', '#FF0000');
 });

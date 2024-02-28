0c1f3c25cdd762069da127dc67961ff13810f8aa
root <dimaryorina@gmail.com>
2024-02-28 09:25:52 -0800

    No JQuery

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const path = process.argv[2];
+
+fs.readFile(path, 'utf8', (err, content) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(content);
+  }
+});
